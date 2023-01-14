31a444d7f78f1489d844964b5922e08cc64dc432
Logesh <logu.siva68@gmail.com>
2023-01-14 12:49:55 +0530

    code format

--- src/app.js
+++ src/app.js
@@ -1,17 +1,16 @@
-const express = require("express");
-const app = express();
-const dotenv = require("dotenv").config();
-const port = process.env.PORT || 5000;
-const authValidate = require("./library/utilities");
-const cors = require("cors");
+const express = require('express')
+const app = express()
+require('dotenv').config()
+const port = process.env.PORT || 5000
+const cors = require('cors')
 
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: '50mb' }))
+app.use(express.urlencoded({ extended: false }))
 
-app.use(cors());
+app.use(cors())
 
-//Cors fix
-/*app.use('/api',(req,res,next)=>{
+// Cors fix
+/* app.use('/api',(req,res,next)=>{
 
   res.header("Access-Control-Allow-Origin", "*");
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
@@ -24,29 +23,10 @@ app.use(cors());
   else {
     next();
   }
-})*/
+}) */
 
-//Un-Auth Routes
-
-app.use("/api/user", require("./routes/security"));
-
-app.use("/api", async (req, res, next) => {
-  console.log("in");
-  try {
-    await authValidate.validateAuth(req, res);
-    if (req.user) next();
-  } catch (err) {
-    res.status(403).json({ status: "error", error: err, message: err.message });
-  }
-});
-
-app.use("/api/clients", require("./routes/clients"));
-app.use("/api/users", require("./routes/user"));
-
-app.use(function (req, res) {
-  res.status(404).json({ message: "Resource Not Found for " + req.url });
-});
+app.use('/api/', require('./routes'))
 
 app.listen(port, () => {
-  console.log(`Server running in Port ${port} `);
-});
+  console.log(`Server running in Port ${port} `)
+})
--- src/data/postgres-db.js
+++ src/data/postgres-db.js
@@ -1,32 +1,31 @@
 // https://www.npmjs.com/package/pg-promise
 // https://github.com/vitaly-t/pg-promise/wiki/Learn-by-Example
 
-const pgpLib = require("pg-promise");
-const monitor = require("pg-monitor");
-const promise = require("bluebird");
-const fs = require("fs");
+const pgpLib = require('pg-promise')
+const monitor = require('pg-monitor')
+const promise = require('bluebird')
 // db config
 const pgconfig = {
   host: process.env.PGHOST,
   port: process.env.PGPORT,
   database: process.env.PGDB,
   user: process.env.PGUSERNAME,
-  password: process.env.PGPASSWORD,
-};
+  password: process.env.PGPASSWORD
+}
 
 // console.log(pgconfig)
 
 // pg-promise initialization options:
-let options = {
-  promiseLib: promise,
+const options = {
+  promiseLib: promise
   // capTX: true, // capitalize transaction commands;
-};
+}
 
-monitor.attach(options); // attaching to all events;
-monitor.setTheme("matrix"); // changing default theme;
+monitor.attach(options) // attaching to all events;
+monitor.setTheme('matrix') // changing default theme;
 
-var pgp = pgpLib(options); // initializing pg-promise;
-pgp.pg.defaults.poolSize = 100;
+const pgp = pgpLib(options) // initializing pg-promise;
+pgp.pg.defaults.poolSize = 100
 
-const db = pgp(pgconfig);
-module.exports = db;
+const db = pgp(pgconfig)
+module.exports = db
